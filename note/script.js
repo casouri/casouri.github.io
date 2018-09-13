e48e96fd67792f921bd6f11a1a13051139e568a5
Yuan Fu <casouri@gmail.com>
2018-09-13 12:33:37 -0400

    Add keybinding code

---
+++ note/script.js
@@ -0,0 +1,5 @@
+// import global script
+var newScript = document.createElement('script')
+newScript.type = 'text/javascript'
+newScript.src = '/script.js'
+document.getElementsByTagName('head')[0].appendChild(newScript)
---
+++ script.js
@@ -0,0 +1,37 @@
+// load mousetrap for key bindings
+var newScript = document.createElement('script')
+newScript.type = 'text/javascript'
+newScript.src = '/misc/mousetrap.js'
+document.getElementsByTagName('head')[0].appendChild(newScript)
+
+// key bindings
+
+function scrollToBottom () {
+  window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight)
+}
+
+function scrollToTop () {
+  window.scrollTo(0, 0)
+}
+
+function flipDown () {
+  scrollBy(0, window.screen.height / 2)
+}
+
+function flipUp () {
+  scrollBy(0, -window.screen.height / 2)
+}
+
+// scroll to bottom
+Mousetrap.bind('option+>', scrollToBottom)
+Mousetrap.bind('G', scrollToBottom)
+// scroll to top
+Mousetrap.bind('option+<', scrollToTop)
+Mousetrap.bind('g g', scrollToTop)
+// scroll down
+Mousetrap.bind('control+v', flipDown)
+Mousetrap.bind('control+d', flipDown)
+// scroll up
+Mousetrap.bind('option+v', flipUp)
+Mousetrap.bind('control+u', flipUp)
+
