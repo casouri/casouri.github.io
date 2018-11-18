cf9cb58d7866a170d45dd798c043d3d0b9f907cb
Yuan Fu <casouri@gmail.com>
2018-11-18 18:28:52 -0500

    Add tag filtering

--- note/script.js
+++
@@ -1,5 +0,0 @@
-// import global script
-var newScript = document.createElement('script')
-newScript.type = 'text/javascript'
-newScript.src = '/script.js'
-document.getElementsByTagName('head')[0].appendChild(newScript)
