88c9c5b8d4868f8fadfdf61e8b045998237f2682
Yuan Fu <casouri@gmail.com>
2021-02-02 02:25:49 -0500

    Update note layout

---
+++ note/script-paginated.js
@@ -0,0 +1,14 @@
+// Copyright: some dude on SO.
+function replaceVerticalScrollByHorizontal(event) {
+  if (Math.abs(event.deltaY) != 0 && Math.abs(event.deltaX) <= 1) {
+    // console.log(event.deltaY);
+    // Manually scroll horizontally instead.
+    window.scroll(window.scrollX + event.deltaY, window.scrollY);
+    // Prevent vertical scroll.
+    event.preventDefault();
+  }
+  return;
+}
+
+window.addEventListener('wheel', replaceVerticalScrollByHorizontal,
+                        {passive: false});
