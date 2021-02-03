6467db30374b04d7c1cb34c49ffda79184e28f88
Yuan Fu <casouri@gmail.com>
2021-02-03 10:36:54 -0500

    Update

--- note/script-paginated.js
+++ note/script-paginated.js
@@ -1,14 +1,16 @@
 // Copyright: some dude on SO.
 function replaceVerticalScrollByHorizontal(event) {
-  if (Math.abs(event.deltaY) != 0 && Math.abs(event.deltaX) <= 1) {
-    // console.log(event.deltaY);
-    // Manually scroll horizontally instead.
-    window.scroll(window.scrollX + event.deltaY, window.scrollY);
-    // Prevent vertical scroll.
-    event.preventDefault();
-  }
-  return;
+    if (Math.abs(event.deltaY) != 0 && Math.abs(event.deltaX) <= 1) {
+        // console.log(event.deltaY);
+        // Manually scroll horizontally instead.
+        window.scroll(window.scrollX + event.deltaY, window.scrollY);
+        // Prevent vertical scroll.
+        event.preventDefault();
+    }
+    return;
 }
 
-window.addEventListener('wheel', replaceVerticalScrollByHorizontal,
-                        {passive: false});
+if (document.matchMedia('screen and (min-width: 1024px)')) {
+  window.addEventListener('wheel', replaceVerticalScrollByHorizontal,
+                          {passive: false});
+}
---
+++ note/script.js
@@ -0,0 +1,4 @@
+function paginate() {
+  document.getElementById('style').setAttribute('href', '../../style-paginate.css');
+   document.getElementById('script').setAttribute('href', '../../script-paginate.css');
+}
