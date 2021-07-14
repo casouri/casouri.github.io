2510fdd50a2dcbd1e5fa0baf29901b10f2500974
Yuan Fu <casouri@gmail.com>
2021-07-14 00:32:29 -0400

    Fix typo

--- note/topics/Programming.html.pm
+++ note/topics/Programming.html.pm
@@ -6,6 +6,6 @@
   ◊title{Programming stories}
 }
 
-As it turns out, it is extremely easy to make mistakes when programming. And is is extremely hard to uncover them. Maybe these stories can help you avoid or uncover a mistake.
+As it turns out, it is extremely easy to make mistakes when programming. And it is extremely hard to uncover them. Maybe these stories can help you avoid or uncover a mistake.
 
 ◊(homepage-titles "Programming")
