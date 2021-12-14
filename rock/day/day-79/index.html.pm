a7c3b97fc4f5c8c7093f052c999434f073ac6f19
Yuan Fu <casouri@gmail.com>
2021-12-13 22:52:31 -0800

    New rock

---
+++ rock/day/day-79/index.html.pm
@@ -0,0 +1,16 @@
+#lang pollen
+
+◊define-meta[date]{<2021-12-13 Mon 11:08>}
+◊define-meta[uuid]{0527724c-5c48-11ec-bd3a-eb3d7b2ebdae}
+
+◊meta{
+  ◊cover{◊cover-img{AATA--LIGHTS.jpg}}
+  ◊artist{AATA}
+  ◊title{}
+  ◊album{LIGHTS}
+  ◊year{2021}
+}
+
+这张也是 Spotify 上听到的，很好听。◊em{Q} 里轻声细语的祈求十分动人。◊em{Identity} 的戏曲唱腔很有趣。◊em{Blame It On The Summer} 活泼轻快。◊em{Slow Flow} 背景里叮～叮～叮～叮～的音效我很喜欢。剩下的◊em{Synapse}，◊em{Do it} 和 ◊em{Lights} 也很棒，◊em{Lights} 和 ◊em{Q} 风格相似，首尾呼应。整张专辑我唯一不喜欢的是 ◊em{Sail on}，里面那个无聊的中央空调暖男声音很扫兴。我本身也不喜欢男女一唱一和的歌，感觉很傻。
+
+最近开车的时候一直循环这几张 JPOP 专辑，我的车可能都烦了哈哈哈。
---
+++ rock/day/src/day-80.html.pm
@@ -0,0 +1,14 @@
+#lang pollen
+
+◊define-meta[date]{<2021-12-13 Mon 11:18>}
+◊define-meta[uuid]{72b23c1a-5c49-11ec-a872-fb2b5d947033}
+
+◊meta{
+  ◊cover{◊cover-img{土岐麻子--passion-blue.jpg}}
+  ◊artist{土岐麻子}
+  ◊title{}
+  ◊album{PASSION BLUE}
+  ◊year{2019}
+}
+
+这应该是最后一张 JPOP 专辑了，最近 Spotify 推荐的专辑已经没有让我眼前一亮的了。这张比较像通常的的流行专辑：一张里有几首有趣的曲目和一些没太大意思的。有趣的有「美しい顔」和 ◊em{RADIO}。稍微有趣的有 ◊em{Ice Cream Talk}，◊em{That Summer} 和 ◊em{Bubble Gum Town}。
