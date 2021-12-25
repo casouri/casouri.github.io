f5ccab21f1707ae4156e82e801644156c1e400ab
Yuan Fu <casouri@gmail.com>
2021-12-25 00:38:39 -0800

    New rock

---
+++ rock/day/day-83/index.html.pm
@@ -0,0 +1,23 @@
+#lang pollen
+
+◊define-meta[date]{<2021-12-23 Thu 21:03>}
+◊define-meta[uuid]{ce331412-6476-11ec-86e2-93008800550d}
+
+◊meta{
+  ◊cover{◊cover-img{紀の国屋バンド--Street Sensation.png}}
+  ◊artist{◊jpns{紀の国屋バンド}}
+  ◊album{Street Sensation}
+  ◊year{1979}
+}
+
+前几天（几个星期前）Xerf Xpec 发了个 ◊link["https://youtu.be/sD1DSdNFxJE"]{◊em{Xerf’s 11 Hour Special}}，我听了，记下来了里面喜欢的单曲。我准备从这些里面挑一些喜欢的，写一个《〈Xerf Xpec 11小时特辑〉特辑》www
+
+《〈特辑〉特辑》的第一篇，就是这张 ◊em{Street Sensation}。整张专辑都很有水平！人声和吉他独奏很精彩，配合很棒。编排也有不少亮点。我最喜欢的是里面比较抒情的几首：◊em{Goodbye Again}，◊em{◊jpns{雨想}}，◊em{You Send Me}。每首都有婉转悠扬的女声和让人毛发皆竖的吉他独奏。主唱的声音有点像◊jpns{山根麻衣}。
+
+◊jpns{紀の国屋バンド}资料很少，只知道主唱是◊jpns{高崎昌子}。专辑的介绍都会说她1979年与◊jpns{竹内まりや}和其他人一起发了 ◊fnref["loft-session"]{◊em{LOFT SESSION}} 出道。
+
+再之后◊jpns{高崎昌子}和其他六个人（不知道是谁）组成了◊jpns{紀の国屋バンド}。这个乐队只发了这一张专辑，有点可惜。不过现在搜◊jpns{高崎昌子}还能找到她的 Facebook 主页，还在活跃演出呢。
+
+◊fndef["loft-session"]{
+  查了查，是张合集专辑，还挺不错的。油管链接：◊link["https://youtu.be/ko1pobee7gA"]{◊em{Loft Sessions Vol.1 (1978) FULL ALBUM HD}}，Discog 链接：◊link["https://www.discogs.com/release/10838984-Various-Loft-Sessions-Vol1"]{◊em{Various – Loft Sessions Vol.1}}。
+}
