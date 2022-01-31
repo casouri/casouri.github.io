2567ff359b409559b832681e411b87543160f680
Yuan Fu <casouri@gmail.com>
2022-01-30 17:00:12 -0800

    New rock

---
+++ rock/day/src/day-84.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊define-meta[date]{<2022-01-30 Sun 16:20>}
+◊define-meta[uuid]{a0d5945e-822b-11ec-8323-574bf038d608}
+
+◊meta{
+  ◊cover{◊cover-img{various--sonny-boy.jpg}}
+  ◊artist{Various}
+  ◊title{}
+  ◊album{◊em{◊link["https://youtu.be/WpSFluMQ_ME"]{Sonny Boy OST}}}
+  ◊year{2021}
+}
+
+去年油管上发现的专辑。专辑的气氛很有趣，像是在一场充满失真和噪点的梦中。曲子风格多变，有离奇梦幻的，也有活泼轻快的，有闲适放松的，有正儿八经的。总体而言是一场不错的旅行，推荐一试。作为整体听的时候，出自不同作曲家，不同风格但又有某种相似主题的曲子一首接一首，感觉很棒。
+
+作曲阵容强大，有◊jpns{銀杏BOYZ}，落日飛車，VIDEOTAPEMUSIC，◊jpns{ザ・なつやすみバンド}，◊jpns{ミツメ}，Ogawa & Tokoro，◊jpns{空中泥棒}，◊jpns{カネヨリマサル}，toe，CONISCH。我只认识一个落日飛車，哈哈。之后有机会了解一下这些乐队，说不定有惊喜。
+
+音乐这么棒，我都有点想看看动画了。中文是《漂流少年》，名字挺不错。看维基说是青春科幻群像剧，看油管评论说有点像 ◊em{FLCL}。说像 ◊em{FLCL} 让我有点犹豫。
+
+PS 看了维基才发现，音乐顾问是◊jpns{渡邊信一郎}，熟人啊。
