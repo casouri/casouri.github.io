d8bd2bfb1ea8d10112ccf3283096653c5ae96813
Yuan Fu <casouri@gmail.com>
2022-02-11 17:30:03 -0800

    Update to next stop

--- next/index.html.pm
+++ next/index.html.pm
@@ -4,17 +4,17 @@
   ◊title{Your Next Stop 下一站}
 }
 
-Maybe less so nowadays, but the Internet used to be an archipelago of personal informational islands. You surf around the net, visit others’ little island, and they points you to other interesting islands to visit.
+Maybe less so nowadays, but the Internet used to be an archipelago of personal hyperspace islands. You surf around the web, visit others’ island, and they points you to other interesting islands to visit.
 
 That’s what I’m going to do here—point you to other people’s islands. If you find my island interesting, you’ll probably find these islands interesting too:
 
-◊link["https://practicaltypography.com"]{◊em{Practical Typography}} is a web book that efficiently introduces typography best-practices. I probably have mentioned it too many times. But if a single soul go ahead and reads it upon my recommendation, and produces better documents, it is well worth it.
-
-◊;{TODO: https://brettgilio.com}
+◊link["https://practicaltypography.com"]{◊em{Practical Typography}} is a web book that efficiently introduces typography best-practices. I probably have mentioned it too many times.
 
 ◊link["https://nullprogram.com"]{◊em{null program}} is a blog about practical programming thoughts and experiences by Chris Wellons. I first knew of his blog from some of its ◊fnref["emacs-blog"]{Emacs blogs}. He also has some very helpful (and fun) tutorials, like ◊link["https://nullprogram.com/blog/2017/08/20/"]{◊em{A Tutorial on Portable Makefiles}}, and ◊link["https://nullprogram.com/blog/2016/11/17/"]{◊em{A Magnetized Needle and a Steady Hand}}.
 
 ◊link["https://type.cyhsu.xyz"]{◊em{NEVERLAND}} 是一个◊fnref["lawyer"]{律师}的技术博客，主要讲一些科技产品，信息消费，还有些别的东西。我看到作者的第一篇文章就是“别的东西”——◊link["https://type.cyhsu.xyz/2021/03/do-not-shout/"]{《不要大喊大叫》}。讲的是目前中文互联网上滥用标点符号，以之突出文字的问题。我还推荐这篇文章，◊link["https://type.cyhsu.xyz/2018/09/understanding-pdf-the-digitalized-paper/"]{《数字世界中的纸张——理解 PDF》}。文章从最基础的概念开始，对计算机技术不熟悉的人也能理解。
+
+◊link["https://www.thetype.com"]{◊em{The Type}}是个讲文字设计的项目。他们有自己的期刊、博客、播客。推荐对中文文字设计排印感兴趣的读者看看他们的◊link["https://www.thetype.com/kongque/"]{孔雀计划}系列和◊link["https://www.thetype.com/kinkido/"]{活字字体基础讲座}系列。
 
 
 ◊fndef["emacs-blog"]{
@@ -22,3 +22,15 @@ That’s what I’m going to do here—point you to other people’s islands. If
 }
 
 ◊fndef["lawyer"]{虽然现在好像不干了。}
+
+◊br{}
+
+Below are some interesting articles that I read recently. Maybe they are interesting to you, too. The top ones are the most recent.
+
+◊dl{
+  ◊dt{
+    ◊link["https://acoup.blog/2022/01/28/collections-rome-decline-and-fall-part-ii-institutions/"]{◊em{Rome: Decline and Fall? Part II: Institutions}}}
+  ◊dd{
+    The author talks about how bad was the decline of the Late Roman Empire into Middle Ages, and whether there was a decline at the first place. Of course, the answer is—as always—“it’s complicated”, but the three part series is still a fun read. The second part that I linked to is especially interesting, it answers many of my long-held questions that my Roman history class in college kind of glossed over.
+  }
+}
