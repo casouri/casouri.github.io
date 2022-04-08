74281193ccd4470c2693d0cc1a81c5d707fd67eb
Yuan Fu <casouri@gmail.com>
2022-04-08 13:59:01 -0700

    Update day 87

--- rock/day/src/day-87.html.pm
+++ rock/day/src/day-87.html.pm
@@ -5,9 +5,11 @@
 
 ◊meta{
   ◊cover{◊cover-img{假假条––时代在召唤.jpg}}
-  ◊artist{假假条}
-  ◊album{◊link["https://youtube.com/playlist?list=OLAK5uy_nX0CIm982DFV0pRYEx6MzlIz8cFHw7umw"]{时代在召唤}}
+  ◊artist{◊trad{假假條}}
+  ◊album{◊link["https://youtube.com/playlist?list=OLAK5uy_nX0CIm982DFV0pRYEx6MzlIz8cFHw7umw"]{◊trad{時代在召喚}}}
   ◊year{2016}
 }
 
-我一般不听金属，尤其他们还喜欢特阴间的艺术风格。这个乐队是我在哔哩哔哩一个中国摇滚乐队介绍视频看到的。那个视频里全是文青软绵绵后摇，一个个有气无力，就这一个乐队很有精神www 这张专辑里民族乐器的用法我很喜欢，尤其是第一首《湘靈鼓瑟》里的打击乐器。这张专辑很容易在 bandcamp 上买到：◊link["https://jajatao.bandcamp.com/album/the-rite-of-spring"]{時代在召喚（The Rite of Spring）}。喜欢的话多多支持！
+我一般不听金属，尤其他们还喜欢特阴间的艺术风格。但这张还挺有意思的。前几天一直在风花雪月，今天来个阴间猛男调和一下也不错。这个乐队是我在哔哩哔哩一个中国摇滚乐队介绍视频看到的。那个视频里全是文青软绵绵后摇，一个个有气无力，就这一个乐队很有精神。这张专辑里民族乐器的用法我很喜欢，尤其是第一首《◊trad{湘靈鼓瑟}》里的打击乐器。突兀的暂停用的也很妙。
+
+这张专辑很容易在 bandcamp 上买到：◊link["https://jajatao.bandcamp.com/album/the-rite-of-spring"]{◊trad{時代在召喚}（The Rite of Spring）}。喜欢的话多多支持！
