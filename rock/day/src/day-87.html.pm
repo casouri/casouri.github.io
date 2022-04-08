fd5156b3d99068c9fceb569f616849f000cd34a6
Yuan Fu <casouri@gmail.com>
2022-04-08 13:48:32 -0700

    New rock

---
+++ rock/day/src/day-87.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+◊define-meta[date]{<2022-04-08 Fri 13:06>}
+◊define-meta[uuid]{5916799a-b777-11ec-9e1a-3b352d04ab79}
+
+◊meta{
+  ◊cover{◊cover-img{假假条––时代在召唤.jpg}}
+  ◊artist{假假条}
+  ◊album{◊link["https://youtube.com/playlist?list=OLAK5uy_nX0CIm982DFV0pRYEx6MzlIz8cFHw7umw"]{时代在召唤}}
+  ◊year{2016}
+}
+
+我一般不听金属，尤其他们还喜欢特阴间的艺术风格。这个乐队是我在哔哩哔哩一个中国摇滚乐队介绍视频看到的。那个视频里全是文青软绵绵后摇，一个个有气无力，就这一个乐队很有精神www 这张专辑里民族乐器的用法我很喜欢，尤其是第一首《湘靈鼓瑟》里的打击乐器。这张专辑很容易在 bandcamp 上买到：◊link["https://jajatao.bandcamp.com/album/the-rite-of-spring"]{時代在召喚（The Rite of Spring）}。喜欢的话多多支持！
