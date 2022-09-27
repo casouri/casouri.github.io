b29fe5b2a1c8919a6b472d41d7ef30e6987cf524
Yuan Fu <casouri@gmail.com>
2022-09-26 20:18:44 -0700

    Update rock

--- rock/day/day-94/index.html.pm
+++ rock/day/day-94/index.html.pm
@@ -10,6 +10,6 @@
   ◊year{1988}
 }
 
-这首是和朋友介绍给我的。我们会玩用音乐标题代替想说的话这种梗。比如有些带主谓宾的歌名，像 ◊em{You Can’t Always Get What You Want}，◊em{Don’t Let Me Down}，或者更抽象一点，像 ◊em{Little Girl Blue}，◊em{Surrender}。「◊jpns{少年は天使を殺す}」这首歌是在什么语境下用的就任君想象了。
+这首是朋友发给我的。我们会玩用音乐标题代替想说的话这种梗。比如有些带主谓宾的歌名，像 ◊em{You Can’t Always Get What You Want}，◊em{Don’t Let Me Down}，或者更抽象一点，像 ◊em{Little Girl Blue}，◊em{Surrender}。超有意思，并且暴露曲库www
 
 我想介绍的其实不是这首歌，而是这张单曲的B面，◊link["https://www.youtube.com/watch?v=5a3BRu5Wor8&t=271s"]{「◊jpns{片想い同盟}」}。整首歌又甜又可爱www。有◊jpns{菊池桃子}这种独特声线，RA MU 商业却上没成功，不太明白。
