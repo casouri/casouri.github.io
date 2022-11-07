3697609d3e595961fe5b6ef9a1a15945aa907a1c
Yuan Fu <casouri@gmail.com>
2022-11-06 16:15:05 -0800

    New day

---
+++ rock/day/day-97/index.html.pm
@@ -0,0 +1,101 @@
+#lang pollen
+
+◊define-meta[date]{<2022-11-05 Sat 20:29>}
+◊define-meta[uuid]{2e0df00e-5d83-11ed-a90b-2f9ac1c6f215}
+
+◊meta{
+  ◊day{97}
+  ◊cover{◊cover-img{people-1--魔法の歌-1.png}}
+  ◊artist{PEOPLE 1}
+  ◊title{◊link["https://youtu.be/7EiQnMK0-R4"]{魔法の歌}}
+  ◊year{2021}
+}
+
+映像：◊link["https://twitter.com/coalowl"]{coalowl}
+協力：どま
+　　　link["https://twitter.com/ayase_aria"]{綾瀬}
+振付：link["https://twitter.com/yamia___72"]{YAMIA}
+
+Music, Words & Programming：Deu
+Co-Produce：Hajime Taguchi
+Mixing Engineer：Ayaka Toki
+Recording Engineer：Yoshiyuki Tai (Sound Base KiTi)
+Mastering Engineer：Tsubasa Yamazaki (Flugel Mastering)
+
+我竟然没发过这首歌。这首是之前在推上还是油管看到的。非常、非常振奋人心的歌。看着画面中的女孩流着泪奔跑，听着背景的合唱，心中的淤塞被一扫而空，心情像鸟一样展翅高飞。音乐和美术竟然有如此难以置信的力量，太感动了！心情不好的时候请一定听一听（配合动画）！
+
+下面是翻译，说实话翻译也看不太懂，不过我也不懂日语，只能这样了www
+
+◊lyrics{
+  ◊jpns{僕はそういう人じゃないから}
+  　　◊trad{我不是那種人啊}
+  ◊jpns{背中をさすってあげられない}
+  　　◊trad{所以無法為你搓背}
+  ◊jpns{膝をついて 人の真似をして 頬を撫でている}
+  　　◊trad{雙膝跪下　模仿人類　撫摸著臉頰}
+
+  ◊jpns{僕はそういう人じゃないから}
+  　　◊trad{我不是那種人啊}
+  ◊jpns{目に映る全てが嘘にみえる}
+  　　◊trad{所以映照在雙眼中的全部看起來都是謊言}
+  ◊jpns{こんな気持ちも本当はバカみたいって分かっている}
+  　　◊trad{我也清楚這種心情實際上十分愚蠢}
+
+  ◊jpns{夕暮れの前哨戦　光が消える頃}
+  　　◊trad{黃昏的前哨戰　在光芒消失之時}
+  ◊jpns{何はなくとも　この声は聞こえるだろうか}
+  　　◊trad{即使一無所有　也能聽見這道聲音吧}
+
+  ◊jpns{忘れるな　愛おしき日々を}
+  　　◊trad{別忘記了　令人憐愛的日子}
+  ◊jpns{全ての理由も知らない奴に語る事情の一つもないのだ}
+  　　◊trad{並沒有想要將所有理由講述給一無所知的傢伙聽啊}
+
+  ◊jpns{見くびるな　愛おしき日々を}
+  　　◊trad{別看不起　令人憐愛的日子}
+  ◊jpns{全部裏切って捨てなくちゃ生きることもままならないのだ}
+  　　◊trad{依然無法背叛切割所有病活下去啊}
+
+  ◊jpns{いつか見失った僕らをちゃんと許してあげられるような}
+  　　◊trad{請唱出婉如能好好地原諒某一天看丟我們般的}
+  ◊jpns{魔法の歌を}
+  　　◊trad{魔法之歌}
+
+  ◊jpns{真夜中の中盤戦　目が慣れる頃}
+  　　◊trad{半夜的中盤戰　在眼睛適應之時}
+  ◊jpns{何はともあれ　この言葉は届くだろうか}
+  　　◊trad{無論發生了什麼　這句話語也會傳達出去的話吧}
+
+  ◊jpns{生き辛いよな　この世の中は}
+  　　◊trad{這個世界還真是　難以生存對吧}
+  ◊jpns{色んな事が分かってきてからはもっと涙がこぼれそうだ}
+  　　◊trad{明白了各種各樣的事情之後總覺得會有更多眼淚奪眶而出}
+
+  ◊jpns{見据えるな　痛ましき日々を}
+  　　◊trad{別看清楚　令人心酸的日子}
+  ◊jpns{悲しみなど蓋をしてゆっくりと腐らせてしまえばいい}
+  　　◊trad{只要敷衍悲傷　讓他慢慢的腐敗就好了}
+
+  ◊jpns{恐れるな　愛おしき日々を}
+  　　◊trad{別害怕　令人憐愛的日子}
+  ◊jpns{世界の終わりは君の左手でその反対側は僕の右手なのだ}
+  　　◊trad{世界末日在你的左手　那反方向是我的右手啊}
+
+  ◊jpns{僕はこういう人だから}
+  　　◊trad{我就是這種人啊}
+  ◊jpns{自分を愛してあげられない}
+  　　◊trad{無法深愛自己}
+  ◊jpns{そんな自分を愛している自分にだって気付いているんだよ}
+  　　◊trad{也注意到了深愛著那樣的自己的自己}
+}◊;lyrics
+
+翻译来源：◊link["https://home.gamer.com.tw/artwork.php?sn=5324013"]{【PEOPLE 1】◊jpns{魔法の歌}【◊trad{中、日、羅歌詞}】}。译者是◊trad{月勳}，留言：
+
+◊quote{◊btrad{
+    日文沒有很好，中文也沒有很好
+    有任何翻譯上的問題歡迎到底下留言或私訊給建議，謝謝◊jpns{◊nobreak{ﾚ(ﾟ∀ﾟ;)ﾍ=З=З=З}}
+    由於是新手，拿取翻譯時請謹慎小心思考，並附上譯者名字與來源，請不要擅自修改翻譯。
+  }
+}
+
+我想留个言点个赞，但是巴哈姆特有什么手机验证通不过，抱歉了老兄www
