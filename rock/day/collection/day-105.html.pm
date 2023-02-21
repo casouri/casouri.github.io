5ae99a5ed48821e4a5478326f8dc2a0b0797e23e
Yuan Fu <casouri@gmail.com>
2023-02-20 19:32:11 -0800

    New rock

---
+++ rock/day/collection/day-105.html.pm
@@ -0,0 +1,102 @@
+#lang pollen
+
+◊define-meta[date]{<2023-02-19 Sun 17:39>}
+◊define-meta[uuid]{75f52e70-b0bf-11ed-bff8-fbe5d4d78384}
+
+◊meta{
+  ◊day{105}
+  ◊cover{◊cover-img{椎名林檎--無罪モラトリアム.jpg}}
+  ◊artist{◊jpns{椎名林檎}}
+  ◊title{◊link["https://www.bilibili.com/video/BV1ar4y1e7x9"]{◊jpns{丸の内サディスティック}}}
+  ◊album{◊jpns{無罪モラトリアム}}
+  ◊year{1999}
+}
+
+写上一篇的时候想到了这首歌。感觉放在前面那一篇下面有点可惜，所以给它单独一篇。一开始听到这首歌是在哔哩哔哩，看到链接的视频。◊jpns{椎名林檎}也是这样认识的。视频真的超酷，晃晃悠悠的镜头，火热的气氛，一脸不爽的表情，露出脖颈的和服，弹舌，小碎步，跺脚，超棒www
+
+这张专辑里其他的歌我也很喜欢，比如◊jpns{「幸福論（悦楽編）」}，扩音器超帅。很难相信这张专辑是1999年出版的！换到今年发售也完全不过时。
+
+封面也很有意思www
+
+◊lyrics{
+  ◊jpns{報酬は入社後並行線で}
+  ◊trans{◊trad{從進入公司後到現在 薪水都呈現水平狀}}
+  ◊jpns{東京は愛せど何もない}
+  ◊trans{◊trad{深愛著東京卻一無所有}}
+  ◊jpns{リッケン620頂戴}
+  ◊trans{◊trad{請給我一把 ◊fnref["Ricken620"]{Ricken620} 吧}}
+  ◊jpns{19万も持っていない}
+  ◊trans{◊trad{雖然我連19萬都付不出來}}
+
+  ◊jpns{御茶の水}
+  ◊trans{◊trad{◊fnref["御茶之水"]{御茶之水}}}
+
+  ◊jpns{マーシャルの匂いで飛んじゃって大変さ}
+  ◊trans{◊trad{在 ◊fnref["Marshall"]{Marshall} 的氣味中飛舞太累人了}}
+  ◊jpns{毎晩絶頂に達して居るだけ}
+  ◊trans{◊trad{只能每晚都在高潮之中度過}}
+  ◊jpns{ラット一つを商売道具にしているさ}
+  ◊trans{◊trad{就把 ◊fnref["Rat"]{Rat} 當成是買賣的道具吧}}
+  ◊jpns{そしたらベンジーが肺に映ってトリップ}
+  ◊trans{◊trad{這樣一來就能把 ◊fnref["Benji"]{Benji} 映在肺中 來場旅行了}}
+
+  ◊jpns{最近は銀座で警官ごっこ}
+  ◊trans{◊trad{最近在◊fnref["銀座"]{銀座}玩起了扮警察遊戲}}
+  ◊jpns{国境は越えても盛者必衰}
+  ◊trans{◊trad{就算跨越了國境 也終究是盛者必衰}}
+  ◊jpns{領収書を書いて頂戴}
+  ◊trans{◊trad{請幫我把收據寫一寫吧}}
+  ◊jpns{税理士なんて就いて居ない後楽園}
+  ◊trans{◊trad{會計師什麼的都不存在的◊fnref["後樂園"]{後樂園}}}
+
+  ◊jpns{将来僧になって結婚して欲しい}
+  ◊trans{◊trad{好希望你將來能成僧然後跟我結婚啊}}
+  ◊jpns{毎晩寝具で遊戯するだけ}
+  ◊trans{◊trad{只能這樣每晚獨自在床上自娛的我}}
+  ◊jpns{ピザ屋の彼女になってみたい}
+  ◊trans{◊trad{好想成為披薩店的那個女孩啊}}
+  ◊jpns{そしたらベンジー あたしをグレッチで殴って}
+  ◊trans{◊trad{這樣的話 Benji 就會用 Gretsch 毆打我了吧}}
+
+  ◊jpns{青 噛んで熟って頂戴}
+  ◊trans{◊trad{就直接在這裡 將這份青澀咬熟吧}}
+  ◊jpns{終電で帰るってば 池袋}
+  ◊trans{◊trad{我還得趕著搭末班電車回家呢 往◊fnref["池袋"]{池袋}}}
+
+  ◊jpns{マーシャルの匂いで飛んじゃって大変さ}
+  ◊trans{◊trad{在 Marshall 的氣味中飛舞太累人了}}
+  ◊jpns{毎晩絶頂に達して居るだけ}
+  ◊trans{◊trad{只能每晚都在高潮之中度過}}
+  ◊jpns{ラット一つを商売道具にしているさ}
+  ◊trans{◊trad{就把 Rat 當成是買賣的道具吧}}
+  ◊jpns{そしたらベンジーが肺に映ってトリップ}
+  ◊trans{◊trad{這樣一來就能把 Benji 映在肺中 來場旅行了}}
+
+  ◊jpns{将来僧になって結婚して欲しい}
+  ◊trans{◊trad{好希望你將來能成僧然後跟我結婚啊}}
+  ◊jpns{毎晩寝具で遊戯するだけ}
+  ◊trans{◊trad{只能這樣每晚獨自在床上自娛的我}}
+  ◊jpns{ピザ屋の彼女になってみたい}
+  ◊trans{◊trad{好想成為披薩店的那個女孩啊}}
+  ◊jpns{そしたらベンジー あたしをグレッチで殴って}
+  ◊trans{◊trad{這樣的話 Benji 就會用 Gretsch 毆打我了吧}}
+
+  ◊fndef["Ricken620"]{◊trad{電吉他的型號。為椎名林檎自己所使用（有在自己的MV中使用過）。}}
+
+  ◊fndef["御茶之水"]{◊trad{東京的車站名稱，位於「丸之內線」上。擁有全日本最大的樂器街。}}
+
+  ◊fndef["Marshall"]{◊trad{來自英國的立體聲音響經典老品牌（旗下也有耳機等相關產品）。}}
+
+  ◊fndef["Rat"]{◊trad{電吉他用的破音效果器。為 Benji（浅井健一）的愛用品。}}
+
+  ◊fndef["Benji"]{◊trad{日本樂團 BLANKEY JET CITY 吉他手浅井健一的暱稱。}}
+
+  ◊fndef["銀座"]{◊trad{東京的車站名稱，位於「丸之內線」上。是十分熱鬧的地區，有很多高級的店（？）。}}
+
+  ◊fndef["後樂園"]{◊trad{東京的車站名稱，位於「丸之內線」上。}}
+
+  ◊fndef["池袋"]{◊trad{東京的車站名稱，為「丸之內線」的終點站，同時也是連結多線的轉乘大站。}}
+
+  转自巴哈姆特的 Mirin：◊link["https://home.gamer.com.tw/artwork.php?sn=5307933"]{◊jpns{【瀬戸乃とと】}◊trad{丸之內虐待狂} ◊jpns{丸の内サディスティック（椎名林檎）}◊trad{【中日歌詞】}}。原文里还有对歌词隐喻很详细的解释，推荐看看！
+  
+}◊;lyrics
