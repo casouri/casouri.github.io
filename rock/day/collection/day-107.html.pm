2b08716fc605c49270a93de6b4666fc68b4041d0
Yuan Fu <casouri@gmail.com>
2023-03-23 17:53:45 -0700

    New rock

---
+++ rock/day/collection/day-107.html.pm
@@ -0,0 +1,18 @@
+#lang pollen
+
+◊define-meta[date]{<2023-03-23 Thu 17:21>}
+◊define-meta[uuid]{e6370ba8-c9d9-11ed-b6e7-734de4b0eb97}
+
+◊meta{
+  ◊day{107}
+  ◊cover{◊cover-img{yourboyfriendsucks!--episode-1.jpg}}
+  ◊artist{你男友係碌葛 yourboyfriendsucks!}
+  ◊album{◊link["https://qiiisnacksrecords.bandcamp.com/album/episode-01"]{第​一​集 Episode 01}}
+  ◊year{2016}
+}
+
+这张专辑怎么遇到的呢？我上个月突发奇想搜了一下“中国地下摇滚”，其中看到了琪琪音像，这么可爱的名字当然要搜一下。然后发现他们有 Bandcamp。Bandcamp 上看到了这张专辑（封面挺好看）。编曲感觉并不复杂，但是就是很好听，能感到欢乐，充满能量www
+
+琪琪音像的标志我挺喜欢，一开始以为是捂着脸的少女，仔细看才发现是手拖着脸，有三只眼睛的少女。没关系，以后我要是开公司，商标就是捂着脸的简笔画短发少女。
+
+琪琪音像还有不少别的专辑，大部份是这种迷幻风的，有几个挺不错的，比如 ◊link["https://qiiisnacksrecords.bandcamp.com/album/idle-archipelago"]{◊em{Idle Archipelago}}，◊link["https://qiiisnacksrecords.bandcamp.com/album/fondle-reissue"]{◊em{Fondle}}（我喜欢这张的封面），还有◊link["https://imdifficult.bandcamp.com/track/kids-live-staged"]{《小​孩》}（我很喜欢这张的封面），但是这张给我的印象比较深，可能因为更大众化吧。
---
+++ rock/day/collection/day-108.html.pm
@@ -0,0 +1,14 @@
+#lang pollen
+
+◊define-meta[date]{<2023-03-23 Thu 17:32>}
+◊define-meta[uuid]{4e319920-c9db-11ed-b5f2-ff10058e9771}
+
+◊meta{
+  ◊day{108}
+  ◊cover{◊cover-img{寺尾紗穂--珈琲.jpg}}
+  ◊artist{◊jpns{寺尾紗穂}}
+  ◊album{◊jpns{珈琲}}
+  ◊year{2013}
+}
+
+这回是 Spotify 倾情推荐，感谢 Spotify。整张专辑最大的特点就是活泼灵动、古灵精怪的钢琴旋律，比如◊jpns{「バルザックおじさん」}、◊jpns{「まちあわせ」}、◊jpns{「小笠原珈琲のうた」}、◊jpns{「ブラジルへ」}。首尾两首◊jpns{「湯布院の朝」}和◊jpns{「インドへ」}则平和舒缓，同样美妙非凡。
