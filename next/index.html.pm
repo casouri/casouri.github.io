efa46dafb6f576470431debd789e03ff20608dd9
Yuan Fu <casouri@gmail.com>
2023-08-17 22:30:18 -0700

    Update

--- next/index.html.pm
+++ next/index.html.pm
@@ -12,7 +12,7 @@ That’s what I’m doing here—point you to other people’s islands. If you f
 
 ◊link["https://nullprogram.com"]{◊em{null program}} is a blog about practical programming thoughts and experiences by Chris Wellons. I first knew of his blog from some of its ◊fnref["emacs-blog"]{Emacs blogs}. He also has some very helpful (and fun) tutorials, like ◊link["https://nullprogram.com/blog/2017/08/20/"]{◊em{A Tutorial on Portable Makefiles}}, and ◊link["https://nullprogram.com/blog/2016/11/17/"]{◊em{A Magnetized Needle and a Steady Hand}}.
 
-◊link["https://type.cyhsu.xyz"]{◊em{NEVERLAND}} 是一个◊fnref["lawyer"]{律师}的技术博客，主要讲一些科技产品，信息消费，还有些别的东西。我看到作者的第一篇文章就是“别的东西”——◊link["https://type.cyhsu.xyz/2021/03/do-not-shout/"]{《不要大喊大叫》}。讲的是目前中文互联网上滥用标点符号，以之突出文字的问题。我还推荐这篇文章，◊link["https://type.cyhsu.xyz/2018/09/understanding-pdf-the-digitalized-paper/"]{《数字世界中的纸张——理解 PDF》}。文章从最基础的概念开始，对计算机技术不熟悉的人也能理解。
+◊link["https://type.cyhsu.xyz"]{NEVERLAND} 是一个◊fnref["lawyer"]{律师}的技术博客，主要讲一些科技产品，信息消费，还有些别的东西。我看到作者的第一篇文章就是“别的东西”——◊link["https://type.cyhsu.xyz/2021/03/do-not-shout/"]{《不要大喊大叫》}。讲的是目前中文互联网上滥用标点符号，以之突出文字的问题。我还推荐这篇文章，◊link["https://type.cyhsu.xyz/2018/09/understanding-pdf-the-digitalized-paper/"]{《数字世界中的纸张——理解 PDF》}。文章从最基础的概念开始，对计算机技术不熟悉的人也能理解。
 
 ◊link["https://www.thetype.com"]{◊em{The Type}}是个讲文字设计的项目。他们有自己的期刊、博客、播客。推荐对中文文字设计排印感兴趣的读者看看他们的◊link["https://www.thetype.com/kongque/"]{孔雀计划}系列和◊link["https://www.thetype.com/kinkido/"]{活字字体基础讲座}系列。
 
