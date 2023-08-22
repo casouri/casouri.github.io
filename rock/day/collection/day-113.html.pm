6e0e2a4ef9a4d98780c600413e96c927eb2f42ed
Yuan Fu <casouri@gmail.com>
2023-08-22 00:29:35 -0700

    New rock

---
+++ rock/day/collection/day-113.html.pm
@@ -0,0 +1,18 @@
+#lang pollen
+
+◊define-meta[date]{<2023-08-21 Mon 23:33>}
+◊define-meta[uuid]{d51eb656-40b5-11ee-b78f-77acd2280ee8}
+
+◊meta{
+  ◊day{113}
+  ◊cover{◊cover-img{山中千尋--utopia.jpg}}
+  ◊artist{◊jpns{山中千尋}}
+  ◊album{◊link["https://youtu.be/ItRMqzZQvGM"]{Utopia}}
+  ◊year{2018}
+}
+
+绝对不是因为封面才推荐的！第二首◊jpns{「乙女の祈り」}我非常喜欢，这种紧凑的鼓点非常棒。因为我经常播第二首曲子，然后 Spotify 就会自动播放之后的曲子，结果后面的曲子听多了也开始喜欢起来了。比如◊jpns{「マンボ」，「白鳥」}。
+
+今天终于买了这张专辑。是在  ◊link["https://ototoy.jp/_/default/p/184977"]{OTOTOY} 上买的。这张也是只能在日本买，废了半天劲才搞定 VPN，可恶。
+
+说起来，仔细看厂牌居然是 Blue Note，真没想到www
