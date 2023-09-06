bb9f93c8918a9adaa674c07a937982136bf6ac75
Yuan Fu <casouri@gmail.com>
2023-09-05 17:07:30 -0700

    Update stream

--- stream/itunes.sh
+++ stream/itunes.sh
@@ -8,9 +8,14 @@ set artist (osascript itunesArtist)
 set cover "$artist"-"$album".jpg
 
 osascript itunesCover
-mv ./cover/cover.jpg ./cover/"$artist"-"$album".jpg
+if test -e cover.png
+    set cover "$artist"-"$album".png
+    mv cover.png cover/$cover
+else
+    mv cover.jpg cover/$cover
+end
 
-echo '{"title":"'$title'","album":"'$album'","artist":"'$artist'","cover":"'./cover/$cover'"}' > info.json
+echo '{"title":"'$title'","album":"'$album'","artist":"'$artist'","cover":"'./$cover'"}' > info.json
 
 rsync ./cover/"$cover" linode:blog/stream
 rsync info.json linode:blog/stream
