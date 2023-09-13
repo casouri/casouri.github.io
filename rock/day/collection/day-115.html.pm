2142ccfd777d01e4b44181c8d67494584e36e020
Yuan Fu <casouri@gmail.com>
2023-09-12 23:15:05 -0700

    New rock

---
+++ rock/day/collection/day-115.html.pm
@@ -0,0 +1,20 @@
+#lang pollen
+
+◊define-meta[date]{<2023-09-12 Tue 21:18>}
+◊define-meta[uuid]{8c57a89a-51ec-11ee-bd08-9f3dee42ca1a}
+
+◊meta{
+  ◊day{115}
+  ◊cover{◊cover-img{SaToA––スリーショット.jpeg}}
+  ◊artist{SaToA}
+  ◊album{◊link["https://youtu.be/W6vb2KWXG20?si=42-2kD4LFLWTZxCF"]{◊jpns{スリーショット}}}
+  ◊year{2018}
+}
+
+这张也是 Spotify 倾情推荐。最先听到的是◊jpns{「夢の島」}，里面有非常舒畅的副歌，很好听。专辑里其他几首也很棒，像是◊jpns{「青草の匂い」}，bitter coffee，Rudolph，◊jpns{「いまあさ」}。
+
+SaToA 是个三人女性乐队。◊fnref["name"]{名字 SaToA 是三个成员名字组成的，唱歌的时候也总是三个人合唱；◊jpns{スリーショット}（three shot）在日语里的意思是三个人的合照}。她们在油管还有个电台频道，居然已经有107期了。三个好朋友一起做音乐、旅游感觉超棒www 真好。
+
+封面我也很喜欢，低饱和度还有点糊糊的，颜色也是很日常舒服的颜色，感觉和乐队的氛围很搭。
+
+◊fndef["name"]{这里看到的：◊link["https://tokyophonic.com/featured/2018/5/31/satoa-"]{SaToA - ◊jpns{スリーショット}}}
