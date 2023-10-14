ce1fc1d2ef596f16fd3654fffeb1732fd57bf67c
Yuan Fu <casouri@gmail.com>
2023-10-13 23:53:09 -0700

    New rock

---
+++ rock/day/collection/day-117.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊define-meta[date]{<2023-10-13 Fri 23:21>}
+◊define-meta[uuid]{e517f1f8-6a59-11ee-846c-7bd4e9ca9a86}
+
+◊meta{
+  ◊day{117}
+  ◊cover{◊cover-img{nine-sparks-riots--the-lotus-and-the-swamp.jpg}}
+  ◊artist{Nine Sparks Riots}
+  ◊title{◊link["https://youtu.be/nJys9Uy7Xis"]{Try Not To Worry}}
+  ◊album{The Lotus and the Swamp}
+  ◊year{2022}
+}
+
+这是有一段时间之前发现的曲子。当时是个晚上，我睡不太着，就把 Spotify 放枕头边，放点音乐听听。突然间这首曲子放出来，用它柔软舒适的温暖空气把我抱在怀里，我瞬间沉醉了，太美了。之后听了这张专辑，发现这首曲子应该只是专辑的 outro，但是我喜欢这首胜过这张专辑里的所有歌。
