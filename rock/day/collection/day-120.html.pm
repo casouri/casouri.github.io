788a87ea278fe0af4bc59a03a590fe20d7d15944
Yuan Fu <casouri@gmail.com>
2023-12-29 23:43:46 -0800

    Rock new day

---
+++ rock/day/collection/day-120.html.pm
@@ -0,0 +1,16 @@
+#lang pollen
+
+◊define-meta[date]{<2023-12-27 Wed 19:43>}
+◊define-meta[uuid]{3dd2ec58-a533-11ee-ac2a-f707679a98fb}
+
+◊meta{
+  ◊day{120}
+  ◊cover{◊cover-img{石若駿--songbook6.jpg}}
+  ◊artist{◊jpns{石若駿}}
+  ◊album{◊link["https://youtu.be/TJ4EGWPLPLY?si=1U7rC3mUVh71hBXg"]{Songbook6}}
+  ◊year{2023}
+}
+
+这张也是在 Spotify 上随手收藏之后过一段时间，发现自己每当不知道听什么的时候会随手点开。也就是说，我的身体在帮我过滤挑选专辑！发现身体的苦心之后认真听了一遍，是很优秀的作品！有凉爽的下午那种闲适的感觉。（最近好像就很喜欢听闲适的音乐。）整张专辑里我最喜欢的可能还是第一首◊jpns{「あちらの空は晴れ 」}。封面也好看，估计是因为封面导致我的身体自动点开这张专辑很多次，然后发现很好听。
+
+音乐家◊jpns{石若駿}还在 BLUE GIANT 的 OST 里出场了。BLUE GIANT 我没看，但我看了  BLUE GIANT SUPREME。很好看！推荐看看！就是看漫画的时候要自己脑补一下声音www
