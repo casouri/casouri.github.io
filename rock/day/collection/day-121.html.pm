aeab64f207d5e5a3efec050b00f78975342e71bb
Yuan Fu <casouri@gmail.com>
2024-01-05 19:52:59 -0800

    New rock

---
+++ rock/day/collection/day-121.html.pm
@@ -0,0 +1,92 @@
+#lang pollen
+
+◊define-meta[date]{<2023-12-29 Fri 23:55>}
+◊define-meta[uuid]{da62e35a-a6e8-11ee-88f0-8b412ce0cc92}
+
+◊meta{
+  ◊day{121}
+  ◊cover{◊cover-img{Panchiko--D>E>A>T>H>M>E>T>A>L.jpg}}
+  ◊artist{Panchiko}
+  ◊album{◊link["https://youtu.be/L3yv7gR_3tU?si=D8HpXuSsSpgzS_dv"]{D>E>A>T>H>M>E>T>A>L}}
+  ◊year{2000}
+}
+
+这张专辑的故事颇为传奇。我一开始在哔哩哔哩上看到的。就是说一几年有个老哥在二手店翻到一张专辑，网上一查啥都没有。这专辑稀有到网上一点点相关的信息都找不到，所以他就上 2chan 问了。2chan 的人让他把 CD 的音轨也发上来，大家一听，居然还挺好听。背景糊糊的噪音也不知道是故意的还是因为 CD 损坏了。
+
+但即使是 2chan 也没能找到专辑的消息。之后有人把音轨传到油管，让更多的人知道了这张专辑和它的神秘背景。渐渐的这张专辑甚至有了自己的爱好者和解谜团体。
+
+专辑背面有四个乐队成员的名字，其中三个都是烂大街的名字，但有一个很少见：Owain。搜寻几年未果之后，人们把目光移到一开始发现这张专辑的二手店，然后还真有人在那附近用脸书找到了一个叫 Owain 的人。然后 Owain 有一天起床发现有人在脸书上联系他，问他是不是 Panchiko 的成员。他估计做梦都没想到过会有一天冒出来个人问他高中的时候组的乐队，然后网上一查发现还有不少粉丝。音乐和互联网真是神奇！
+
+我最喜欢专辑的第一首歌，里面的的音效和采样我很喜欢，重复的旋律很棒，“don’t play the track”那里也很有意思。背景忽闪忽闪的声音听起来像是在水中飞行，喜欢。我记得有某种专业名词描述这种声音，但我没印象了。
+
+另外值得一提的是主唱的声音好有少年感，现实里重新录制这首歌的时候已经是个大叔了吧，但听起来还像是个高中生。
+
+最后，封面我很喜欢www
+
+◊lyrics{
+  She’s forgotten her car keys
+  And the flowers for her friends
+  They say she’s a harpy
+  But she is on her meds
+
+  ’Cause you’re holding onto someone who’s special
+  Educating someone who’s mental
+  Giving the fat pigs their facial
+  We all listen to death metal
+
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t play the track
+  Do-Do-Don’t-, Do-Do-Do-Don’t-
+  Do-Do-Don’t-, Do-Do-Do-Don’t play the track
+
+  She’s mates with Roy Fokker
+  And he’s completely faced
+  She’s in with the rockers
+  But it’s not to her taste
+
+  ’Cause you’re holding onto someone who’s special
+  Educating someone who’s mental
+  Giving the fat pigs their facial
+  We all listen to death metal
+
+  Let it go
+  Let it go
+  Let it go
+  Let it go
+  No one knows
+  Where she goes
+  No one knows
+  Where she goes
+  La-la-la, la-la
+  La-la-la-la, la-la
+  La-la-la-la, la-la
+  La-la-la-la, la-la
+  La-la-la, la-la
+  La-la-la-la, la-la
+
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+  Do-Don’t- Do-Don’t- Do-Don’t- Do-Don’t- Don’t-
+
+  Source: ◊link["https://www.azlyrics.com/lyrics/panchiko/deathmetal.html"]{AZ Lyrics}
+}◊;lyrics
