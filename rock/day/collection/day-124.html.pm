357f6dcca408efed8aaa0cc7bfe825d3644f5742
Yuan Fu <casouri@gmail.com>
2024-05-08 22:44:46 -0700

    New rock

---
+++ rock/day/collection/day-124.html.pm
@@ -0,0 +1,19 @@
+#lang pollen
+
+◊define-meta[date]{<2024-05-08 Wed 22:09>}
+◊define-meta[uuid]{4a2a104c-0dc2-11ef-abf1-cf6dad9a8895}
+
+◊meta{
+  ◊day{124}
+  ◊cover{◊cover-img{chocolate-lips--chocolate-lips.jpg}}
+  ◊artist{Chocolate Lips}
+  ◊album{Chocolate Lips}
+  ◊title{◊link["https://youtu.be/QHFcTO0TAsE"]{Foolish Girl}}
+  ◊year{1984}
+}
+
+今天来点轻松的，我忘了在哪发现的了，可能是油管？这首歌我说不出来啥，就是挺好听www 整张专辑我还没有认真听过，但好像没有比这首更好听的了。
+
+我偶然找到了这张 Chocolate Lips 的合照，是不是很有感觉：
+
+◊cover-img{etc/chocolate-lips.jpg}
