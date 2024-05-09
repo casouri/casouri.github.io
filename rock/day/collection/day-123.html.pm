4ea7e62ed9c88a8fb5ae4f5fd2eb54b4ce4832fa
Yuan Fu <casouri@gmail.com>
2024-05-08 22:06:22 -0700

    New rock

---
+++ rock/day/collection/day-123.html.pm
@@ -0,0 +1,36 @@
+#lang pollen
+
+◊define-meta[date]{<2024-05-08 Wed 21:59>}
+◊define-meta[uuid]{f2c67404-0dc0-11ef-a67a-bf3f887c4974}
+
+◊meta{
+  ◊day{123}
+  ◊cover{◊cover-img{astrid-sonne--great-doubt.jpg}}
+  ◊artist{Astrid Sonne}
+  ◊title{◊link["https://astridsonne.bandcamp.com/album/great-doubt"]{Do you wanna}}
+  ◊album{Great Doubt}
+  ◊year{2024}
+}
+
+这首歌给我的感觉很神奇，沉重的鼓点，厚重的钢琴和弦，给我一种漆黑沉重的夜幕缓缓落下盖在我身上的感觉。我就是很喜欢听里面的钢琴和弦。感觉，damn，好丰满，好厚重。
+
+◊lyrics{
+You, you look at me
+You, you look at me
+
+Do you wanna have a baby?
+Do you wanna have a baby?
+
+I think to myself
+I think to myself
+
+Do you wanna have a baby?
+Do you wanna bring people into this world?
+
+People
+
+Do you wanna have a baby?
+I really don’t know
+Do you wanna have a baby?
+I really don’t know
+}◊;lyrics
