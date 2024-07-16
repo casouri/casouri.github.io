882e513efc1051fdecb26b7d29babfdfc242a05c
Yuan Fu <casouri@gmail.com>
2024-07-16 11:52:45 -0700

    New rock

---
+++ rock/day/collection/day-125.html.pm
@@ -0,0 +1,89 @@
+#lang pollen
+
+◊define-meta[date]{<2024-07-16 Tue 11:18>}
+◊define-meta[uuid]{ca66cb52-439f-11ef-807c-87a23e9e1d43}
+
+◊meta{
+  ◊day{125}
+  ◊cover{◊cover-img{p3r.jpg}}
+  ◊artist{Lotus Juice · ◊jpns{高橋あず美} · ATLUS Sound Team · ATLUS GAME MUSIC}
+  ◊title{◊link["https://youtu.be/6Ve41PhGKRI?si=mj202nY-IuWJaYXo"]{Color Your Night}}
+  ◊album{Persona 3 Reload Original Soundtrack}
+  ◊year{2024}
+}
+
+我完全是为了这首歌玩的 P3R。在哔哩哔哩上看到这首歌以后惊为天人，太潮了，太舒服了，大脑像是被音乐按摩了一样。进游戏到了晚上的街上，这首歌悄然响起，身上汗毛倒竖。
+
+专辑里的 Full Moon Full Life，Want to Be Close，Deep Breath Deep Breath 也都非常棒，游戏里听到的时候就十分享受，出来单独听也别有风味。
+
+◊lyrics{
+Time to shine and I'm down to ride
+I'm gonna drive until the break of dawn, wilding out
+I'm on a mission, grinding out
+Whatever it is I gotta do
+Riding out until the day I meet you, I'm riding out
+I look up in the sky
+Ask a lot of: Why?
+But never let it faze me, I'm keeping good vibes
+When I'm on the road, watch out for all surprises
+What it provides us
+Good or bad but nothing unties us
+
+Another wander in the night
+Let me paint the view
+Color the town with my light
+For every moment shared with you
+Not out in the day
+But never fully gone
+Going to be back again
+Until the coming of the dawn
+
+(Hey)
+For all the days ahead of us, put your hands up
+Wave 'em side to side for good luck, uh
+(Hey)
+For all the nights like this, put your hands up
+Love the moment even from dusk, yup
+(Hey)
+You always got my back, and of course I got your back
+Don't you worry 'bout that, now
+(Hey)
+It's been a good day, good night
+It's gonna be a good day, good night
+
+Two hands meet at zenith
+Nothing comes between us
+Dancin' like ballerinas
+Under the moonlight all night long
+How these plain and simple scenes get painted so vividly by you
+I just wanna stay with you
+
+Around the world I slowly dance
+Whenever our eyes meet
+You know you leave me in a trance
+It means everything to me
+Looking down below
+The one I seek to find
+No matter where you might go
+Remember, you'll be on my mind
+
+Always watching over me, somehow you make me feel invincible
+Power to my soul, yup
+I'm at easy with things that used to bother me
+'Cause your existence just lights me up
+You always got my back and of course I got your back
+Don't you worry 'bout that, now
+It's been a good day, good night
+It's gonna be a good day, good night
+
+Within the inner heart, I plead
+For you to talk to me
+I wanna hear all the good and bad
+The new stories you have
+As I drift across the sky
+The hour hand turns high
+So, goodnight
+Let me sing a lullaby now
+I'll be singing, baby
+
+Source: ◊link["https://www.letras.com/persona-3-reload/color-your-night/"]{letras}}◊;lyrics
