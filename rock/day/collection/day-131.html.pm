5e4c8de9e3034ca281f73ca036f171ecb8e6f7f3
Yuan Fu <casouri@gmail.com>
2025-02-03 15:38:33 -0800

    New rock

---
+++ rock/day/collection/day-131.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊define-meta[date]{<2025-01-30 Thu 13:44>}
+◊define-meta[uuid]{6501e4b8-df53-11ef-84a9-236950a42423}
+
+◊meta{
+  ◊day{131}
+  ◊cover{◊cover-img{笠井紀美子--トーキョー・スペシャル.jpg}}
+  ◊artist{◊jpns{笠井紀美子}}
+  ◊title{◊jpns{やりかけの人生}}
+  ◊album{◊jpns{トーキョー・スペシャル} (Tokyo Special)}
+  ◊year{1977}
+}
+
+在油管上遇见的，像往常一样随便点进去试试，从第一秒就被擭住心神。如此旖旎、梦幻、留恋，请好好享受。
