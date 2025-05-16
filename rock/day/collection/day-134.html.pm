c9eb101f0a36931cedb5f0ba9e0c44fb70802a35
Yuan Fu <casouri@gmail.com>
2025-05-15 21:09:45 -0700

    New rock

---
+++ rock/day/collection/day-134.html.pm
@@ -0,0 +1,16 @@
+#lang pollen
+
+◊define-meta[date]{<2025-05-15 Thu 19:37>}
+◊define-meta[uuid]{e9d8e576-31fe-11f0-b7a9-3e26bb447f13}
+
+◊meta{
+  ◊day{134}
+  ◊cover{◊cover-img{tamtam--we-are-the-sun.jpg}}
+  ◊artist{TAMTAM}
+◊album{◊link["https://tamtam.bandcamp.com/album/we-are-the-sun-home-edition"]{We Are the Sun!}}
+  ◊year{2020}
+}
+
+本来没打算介绍这张，但是自从不知道多少个月前买了以后，时不时地听，越听越感觉有 分享的价值。感觉跟 ◊link["/rock/day/collection/day-112.html"]{Day 112} 的 CRCK/LCKS 很像：非常流行，可能不太耐听，但是时不时听一下总会感觉很好听很有趣。我最喜欢的是 Worksong! Feat. ◊jpns{鎮座}DOPENESS，Flamingos，Beautiful Bad Dream，和 Summer Ghost。
+
+他们的另一张专辑 ◊link["https://tamtam.bandcamp.com/album/ramble-in-the-rainbow"]{Ramble In The Rainbow} 我也买了，也很喜欢。Doors 和 Nue 很好听。
---
+++ rock/day/collection/day-135.html.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊define-meta[date]{<2025-05-15 Thu 20:21>}
+◊define-meta[uuid]{e06099d4-3204-11f0-8a0c-3e26bb447f13}
+
+◊meta{
+  ◊day{135}
+  ◊cover{◊cover-img{吉田美奈子--扉の冬.jpg}}
+  ◊artist{◊jpns{吉田美奈子}}
+  ◊title{◊link["https://youtu.be/IDTSSkc4D_Y"]{◊jpns{扉の冬}}}
+  ◊album{◊jpns{扉の冬}}
+  ◊year{1973}
+}
+
+Reddit 上看到一个帖子推荐◊jpns{吉田美奈子}的◊jpns{「愛は思うまま LET’S DO IT」}，去 Spotify 听了，但没有我很喜欢的。但是好在没有放弃，又播了这张。◊jpns{「扉の冬」}里人声和钢琴耳鬓厮磨，翩翩起舞，太美妙了。
+
+另外，另一张专辑 LIGHT’N UP 里的 MORNING PRAYER 我也很喜欢。这两张专辑里大部份的歌都很好听，都拿来听听吧！LIGHT’N UP 唯一的问题是封面太辣眼了，几个字母跟小学生拼字一样随便摆，还在头两边各放一个，加倍伤害。
